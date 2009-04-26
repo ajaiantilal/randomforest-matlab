8375e00abdf152268ff398ad00c92c30351ac8b3
abhirana <abhirana@ab8f585e-2f68-11de-8e0c-53a27eea117e>
2009-04-26 03:16:50 +0000

    working version for 64bit windows

--- RF_Class_C/compile_windows.m
+++ RF_Class_C/compile_windows.m
@@ -13,8 +13,8 @@ function compile_windows
     fprintf('If it doesnt work then use cygwin+g77 (or gfortran) to recompile rfsub.f\n');
 
     if strcmp(computer,'PCWIN64')
-        mex  -DMATLAB  -output mexClassRF_train   src/classRF.cpp src/classTree.cpp src/cokus.cpp precompiled_rfsub/win64/rfsub.o src/mex_ClassificationRF_train.cpp   src/rfutils.cpp 
-        mex  -DMATLAB  -output mexClassRF_predict src/classRF.cpp src/classTree.cpp src/cokus.cpp precompiled_rfsub/win64/rfsub.o src/mex_ClassificationRF_predict.cpp src/rfutils.cpp 
+        mex  -DMATLAB -DWIN64 -output mexClassRF_train   src/classRF.cpp src/classTree.cpp src/cokus.cpp precompiled_rfsub/win64/rfsub.o src/mex_ClassificationRF_train.cpp   src/rfutils.cpp 
+        mex  -DMATLAB -DWIN64 -output mexClassRF_predict src/classRF.cpp src/classTree.cpp src/cokus.cpp precompiled_rfsub/win64/rfsub.o src/mex_ClassificationRF_predict.cpp src/rfutils.cpp 
     elseif strcmp(computer,'PCWIN')
         mex  -DMATLAB -output mexClassRF_train   src/classRF.cpp src/classTree.cpp src/cokus.cpp precompiled_rfsub/win32/rfsub.o src/mex_ClassificationRF_train.cpp   src/rfutils.cpp 
         mex  -DMATLAB -output mexClassRF_predict src/classRF.cpp src/classTree.cpp src/cokus.cpp precompiled_rfsub/win32/rfsub.o src/mex_ClassificationRF_predict.cpp src/rfutils.cpp 
